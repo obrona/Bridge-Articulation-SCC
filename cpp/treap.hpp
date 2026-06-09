9420ea8845688f1901072accbf0926628453c661
obrona <tlim8772@gmail.com>
2026-06-09 12:02:53 +0800

    change treap code

--- cpp/treap.hpp
+++ cpp/treap.hpp
@@ -1,17 +1,119 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename T>
+template <typename T, typename Compare = std::less<T>>
 struct TreapNode {
+    static mt19937 rng;
+    
     T val;
     int cnt = 1;
     int prior;
-    TreapNode *l = nullptr, *r = nullptr;
+    TreapNode<T, Compare> *l = nullptr, *r = nullptr;
 
-    TreapNode(T val, int prior): val(val), prior(prior) {}
+    TreapNode(T val): val(val), prior(rng()) {}
 };
 
 template <typename T, typename Compare = std::less<T>>
+array<TreapNode<T, Compare>*, 2> split(TreapNode<T> *t, T val, const Compare& cmp = Compare()) {
+    if (t == nullptr) return {nullptr, nullptr};
+    if (cmp(t->val, val)) {
+        auto [l, r] = split(t->r, val);
+        t->r = l;
+        return {t, r};
+    } else {
+        auto [l, r] = split(t->l, val);
+        t->l = r;
+        return {l, t};
+    }
+}
+
+// return less than, equal, more than
+template<typename T, typename Compare = std::less<T>>
+array<TreapNode<T>*, 3> split_equal(TreapNode<T> *t, T val, const Compare& cmp = Compare()) {
+    if (t == nullptr) return {nullptr, nullptr, nullptr};
+
+    if (!cmp(t->val, val) && !cmp(val, t->val)) {
+        auto l = t->l, r = t->r;
+        t->l = t->r = nullptr;
+        return {t->l, t, t->r};
+    }
+
+    if (cmp(t->val, val)) {
+        auto [l, e, r] = split_equal(t->r, val, cmp);
+        t->r = l;
+        return {t, e, r};
+    } else {
+        auto [l, e, r] = split_equal(t->l, val, cmp);
+        t->l = l;
+        return {l, e, t};
+    }
+}
+
+template <typename T, typename Compare = std::less<T>>
+TreapNode<T, Compare>* merge(TreapNode<T, Compare> *l, TreapNode<T, Compare> *r, const Compare& cmp = Compare()) {
+    if (l == nullptr) return r;
+    if (r == nullptr) return l;
+
+    // if l->val and r->val are the same, then l cannot have any right children and r cannot have any left children.
+    if (!cmp(l->val, r->val) && !cmp(r->val, l->val)) {
+        l->prior = max(l->prior, r->prior);
+        l->cnt += r->cnt;
+        l->r = r->r;
+        delete r;
+        return l;
+    }
+
+    if (l->prior >= r->prior) {
+        l->r = merge(l->r, r, cmp);
+        return l;
+    } else {
+        r->l = merge(l, r->l, cmp);
+        return r;
+    }
+}
+
+
+template <typename T, typename Compare = std::less<T>>
+void insert(TreapNode<T, Compare>*& root, T val, const Compare& cmp = Compare()) {
+    auto [l, r] = split<T, Compare>(root, val);
+    t = merge(l, merge(new TreapNode(val), r));
+}
+
+template <typename T, typename Compare = std::less<T>>
+void erase(TreapNode<T, Compare>*& root, T val, const Compare& cmp = Compare()) {
+    auto [l, e, r] = split_equal<T, Compare>(root, val);
+    t = merge(l, r);
+}
+
+template<typename T, typename Compare = std::less<T>>
+TreapNode<T, Compare>* unite(TreapNode<T, Compare>* t1, TreapNode<T, Compare>* t2) {
+    if (t1 == nullptr) return t2;
+    if (t2 == nullptr) return t1;
+
+    if (t1->prior < t2->prior) swap(t1, t2);
+    
+    auto [l, e, r] = split(t2, t1->val);
+    if (e != nullptr) {
+        t1->cnt += e->cnt;
+        delete e;
+    }
+    t1->l = unite(t1->l, l);
+    t1->r = unite(t1->r, r);
+    return t1;
+}
+
+template<typename T, typename Compare = std::less<T>>
+void clean(TreapNode<T, Compare>* t) {
+    if (t == nullptr) return;
+    clean(t->l);
+    clean(t->r);
+    delete t;
+}
+
+
+
+
+/*template <typename T, typename Compare = std::less<T>>
 struct Treap {
     mt19937 rng;
     Compare cmp;
@@ -19,7 +121,7 @@ struct Treap {
 
     bool equal(T t1, T t2) {
         return !cmp(t1, t2) && !cmp(t2, t1);
-    }
+    } 
 
     // here left split is values < val, right split is >= val.
     // to follow cpp convention of less than.
@@ -112,5 +214,8 @@ struct Treap {
 
         helper(root);
     }
-   
-};
+};*/
+
+
+
+//template<typename T, typename Compare = std::less<T>>
--- cpp/treap_test.cpp
+++ cpp/treap_test.cpp
@@ -2,8 +2,8 @@
 #include "treap.hpp"
 
 template <typename T, typename Compare = std::less<T>>
-void validate(TreapNode<T> *root) {
-    if (root == nullptr) return;
+void validate(TreapNode<T, Compare>& t) {
+    if (t == nullptr) return;
 
     Compare cmp;
     vector<T> inorder;
@@ -20,31 +20,34 @@ void validate(TreapNode<T> *root) {
         }
     };
 
-    dfs(root);
+    dfs(t);
 
     for (int i = 0; i < inorder.size() - 1; i++) {
         assert(cmp(inorder[i], inorder[i + 1]));
     }
 }
 
-void test1() {
-    Treap<int> treap;
-    for (int i = 0; i < 100000; i++) treap.insert(1);
-    assert(treap.root->cnt == 100000);
+template <typename T, typename Compare = std::less<T>>
+void validate(TreapNode<T>* t, const vector<T>& expected) {
+    if (treap.root == nullptr) return;
+
+    Compare cmp;
+    vector<T> inorder;
+
+    function<void(TreapNode<T>*)> dfs = [&] (auto t) {
+        if (t->l != nullptr) {
+            assert(t->l->prior <= t->prior);
+            dfs(t->l);
+        }
+        inorder.push_back(t->val);
+        if (t->r != nullptr) {
+            assert(t->r->prior <= t->prior);
+            dfs(t->r);
+        }
+    };
+
+    dfs(t);
+
+    assert(inorder == expected);
 }
 
-void test2() {
-    vector<int> nums(100000);
-    iota(nums.begin(), nums.end(), 0);
-    shuffle(nums.begin(), nums.end(), mt19937());
-
-    Treap<int> treap;
-    for (int x : nums) treap.insert(x);
-    validate<int>(treap.root);
-
-}
-
-int main() {
-    test1();
-    test2();
-}
